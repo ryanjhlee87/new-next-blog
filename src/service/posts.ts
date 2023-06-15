e69f10b243d52e2e967fc133ef28b68a4ef0c942
ryanjhlee87 <108978592+ryanjhlee87@users.noreply.github.com>
2023-06-15 16:15:15 +0900

    Add component structure

---
+++ src/service/posts.ts
@@ -0,0 +1,26 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+export type Post = {
+  title: string;
+  description: string;
+  date: Date;
+  category: string;
+  path: string;
+  featured: boolean;
+};
+
+export async function getAllPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), 'data', 'posts.json');
+
+  return readFile(filePath, 'utf-8')
+    .then<Post[]>(data => JSON.parse(data))
+    .then(posts => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getFeaturedPosts(): Promise<Post[]> {
+  const posts = await getAllPosts();
+  const featuredPosts = posts.filter(post => post.featured);
+
+  return featuredPosts;
+}
